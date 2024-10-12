8526f42ff44187a0be5597a1176915c138547db4
Tom <tom.liu.jt@gmail.comm>
2024-10-12 21:12:29 +1100

    Added server.js w/ basic setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,9 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send("Server started");
@@ -9,5 +12,5 @@ app.get('/', (req, res) => {
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log(`Serve at http://localhost:${port}`);
+  console.log(`Server at http://localhost:${port}`);
 });
