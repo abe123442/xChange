b900722063a3417ffca35fdbb458a2200283ad1e
Tom <tom.liu.jt@gmail.comm>
2024-10-14 18:49:31 +1100

    Added server route

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,9 +5,30 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+// ====================================================================
+// SERVER ROUTES BELOW ================================================
+// ====================================================================
+
 app.get('/', (req, res) => {
   res.send("Server started");
 });
+
+app.get('/:infoid', (req, res) => {
+  const infoid = parseInt(req.params.infoid);
+  const token = req.header('token');
+
+  try {
+    const response = viewInfo(token, infoid);
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(error.status).json({ error: error.message });
+  }
+});
+
+
+// ====================================================================
+// SERVER ROUTES ABOVE ================================================
+// ====================================================================
 
 const port = process.env.PORT || 5000;
 
