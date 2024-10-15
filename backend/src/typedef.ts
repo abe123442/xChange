98d152edaa6e50c46ca0c354553be8b9e62b4e6a
Jaiden Gill <z5478407@ad.unsw.edu.au>
2024-10-15 12:38:42 +1100

    created ts types

--- backend/src/typedef.ts
+++ backend/src/typedef.ts
@@ -1,11 +1,32 @@
 // ENUMS
 export enum category {
-  //TODO: put the Super Partner, high demand stuff here
+  SUPERPARTNER,
+  HIGHCAPACITY,
+  HIGHDEMAND
 }
 
 // INTERFACES
 export interface Profile {
+  id: number,
+  name: string,
+  desc: string,
+  country: string,
+  scope: string,
+  category: category,
+  minWam: number,
+  fullTimeLoad: number,
+  link: string,
+  img: string,
+  rating: number,
+  numRates: number,
+  comments: Array<number>
+}
 
+export interface Comment {
+  id: number,
+  title: string,
+  desc: string,
+  rating: number
 }
 
 export interface Data {
