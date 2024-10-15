bebad2f9be881d5fa9ff830b3fa40e93b6d709f4
Tom <tom.liu.jt@gmail.comm>
2024-10-15 12:16:26 +1100

    Began converting to ts

---
+++ backend/src/typedef.ts
@@ -0,0 +1,13 @@
+// ENUMS
+export enum category {
+  //TODO: put the Super Partner, high demand stuff here
+}
+
+// INTERFACES
+export interface Profile {
+
+}
+
+export interface Data {
+  profiles: Profile;
+}
