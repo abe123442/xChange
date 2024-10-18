ccadf21bda4e6771de4a48df54939e00af40b6b3
Tom <tom.liu.jt@gmail.comm>
2024-10-18 17:19:52 +1100

    Prevented stupid caching

--- frontend/updated-frontend/app/profile/[id]/page.tsx
+++ frontend/updated-frontend/app/profile/[id]/page.tsx
@@ -4,7 +4,7 @@ import { Profile } from '@/components/profile/profile';
 import { Comment } from '@/components/profile/comment';
 import { BACKEND_URL } from '@/lib/utils';
 
-export const dynamic = 'force-dynamic';
+export const revalidate = 0;
 
 export default async function Page({ params }: { params: { id: string }}) {
   const { id } = params;
@@ -13,7 +13,7 @@ export default async function Page({ params }: { params: { id: string }}) {
 
   const commentData = await fetch(BACKEND_URL + `/profile/${id}/comments`);
   const comments = await commentData.json();
-  
+
   return (
     <>
       <NavBar />
