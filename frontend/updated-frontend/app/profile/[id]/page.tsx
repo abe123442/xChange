8365cdfa00e61e9f3e2d34897761ef53f5bac68f
Tom <tom.liu.jt@gmail.comm>
2024-10-18 17:16:15 +1100

    Added force-dynamic to prevent caching

--- frontend/updated-frontend/app/profile/[id]/page.tsx
+++ frontend/updated-frontend/app/profile/[id]/page.tsx
@@ -4,6 +4,8 @@ import { Profile } from '@/components/profile/profile';
 import { Comment } from '@/components/profile/comment';
 import { BACKEND_URL } from '@/lib/utils';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Page({ params }: { params: { id: string }}) {
   const { id } = params;
   const profileData = await fetch(BACKEND_URL + `/profile/${id}`);
@@ -11,7 +13,7 @@ export default async function Page({ params }: { params: { id: string }}) {
 
   const commentData = await fetch(BACKEND_URL + `/profile/${id}/comments`);
   const comments = await commentData.json();
-  console.log(comments);
+  
   return (
     <>
       <NavBar />
