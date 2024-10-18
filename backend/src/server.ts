51d7a94527e438b903fe500f2bf6ea60f9c60c97
Tom <tom.liu.jt@gmail.comm>
2024-10-18 21:29:01 +1100

    Changed port back to 5000

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -233,7 +233,7 @@ app.delete('/comments/:commentid/downvote', (req: Request, res: Response) => {
 // SERVER ROUTES ABOVE ================================================
 // ====================================================================
 
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 
 app.use(errorHandler());
 
